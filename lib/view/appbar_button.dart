6eb60ed3e5a37f589140ac5c1e6906fe402a9286
wdevore <william.quartz@gmail.com>
2021-09-09 18:38:37 -0400

    removed older code

--- lib/view/appbar_button.dart
+++ lib/view/appbar_button.dart
@@ -33,14 +33,8 @@ class _AppBarButtonState extends State<AppBarButton> {
 
   @override
   Widget build(BuildContext context) {
-    // final provider = ChoiceProvider.of(context);
-
     return TextButton(
       onPressed: () {
-        // setState(() {
-        // print('id $id');
-        // provider.buttonModel.id = id;
-        // });
         choiceCallback(id);
       },
       child: Text(caption),
@@ -52,50 +46,3 @@ class _AppBarButtonState extends State<AppBarButton> {
     );
   }
 }
-
-
-// class AppBarButton extends StatefulWidget {
-//   AppBarButton({
-//     Key? key,
-//     required this.id,
-//     required this.caption,
-//     required this.choiceCallback,
-//     required this.chosenId,
-//   }) : super(key: key);
-
-//   final AppBarChoise id;
-//   final String caption;
-//   final ChoiceCallback choiceCallback;
-//   final AppBarChoise chosenId;
-
-//   @override
-//   _AppBarButtonState createState() =>
-//       _AppBarButtonState(id, caption, choiceCallback, chosenId);
-// }
-
-// class _AppBarButtonState extends State<AppBarButton> {
-//   AppBarChoise id;
-//   String caption;
-//   ChoiceCallback choiceCallback;
-//   AppBarChoise chosenId;
-
-//   _AppBarButtonState(this.id, this.caption, this.choiceCallback, this.chosenId);
-
-//   Color _activeColor() =>
-//       widget.chosenId == id ? Colors.orange : Colors.black12;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return TextButton(
-//       onPressed: () {
-//         choiceCallback(id);
-//       },
-//       child: Text(caption),
-//       style: TextButton.styleFrom(
-//         padding: const EdgeInsets.only(left: 10, right: 10),
-//         primary: Colors.white,
-//         backgroundColor: _activeColor(),
-//       ),
-//     );
-//   }
-// }
