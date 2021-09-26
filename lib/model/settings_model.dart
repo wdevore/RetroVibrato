d26bd11c8dece8fd6d4bc345a9786f9765ef99ff
wdevore <william.quartz@gmail.com>
2021-09-25 23:34:59 -0400

    added remaining json settng fields

--- lib/model/settings_model.dart
+++ lib/model/settings_model.dart
@@ -91,17 +91,20 @@ class HighPassFilterSettings {
 }
 
 class AppSettings {
+  final Field name = Field.noRange("", "Name");
   final Field sfxrFile = Field.noRange("", "Sfxr File");
   final Field waveFile = Field.noRange("", "Wave File");
   final Field destEmail = Field.noRange("", "Destination Email");
   final Field autoplay = Field.noRange(true, "Auto Play");
-  final Field sampleRate = Field.noRange(SampleRate.KHz44, "Auto Play");
+  final Field sampleRate = Field.noRange(SampleRate.KHz44, "Sample Rate");
+  final Field sampleSize = Field.noRange(8, "Sample Size");
   final Field volume = Field.noRange(0.5, "Auto Play");
+  final Field generator = Field.noRange(Generator.None, "None");
+  final Field wave = Field.noRange(WaveForm.None, "None");
 }
 
 class SettingsModel {
   final appSettings = AppSettings();
-  final envelopeSettings = EnvelopeSettings();
   final frequencySettings = FrequencySettings();
   final vibratoSettings = VibratoSettings();
   final arpeggiationSettings = ArpeggiationSettings();
@@ -110,14 +113,111 @@ class SettingsModel {
   final flangerSettings = FlangerSettings();
   final lowPassFilterSettings = LowPassFilterSettings();
   final highPassFilterSettings = HighPassFilterSettings();
+  final envelopeSettings = EnvelopeSettings();
 
   void fromJson(String json) {
     var jSettings = jsonDecode(json);
+
+    appSettings.name.value = jSettings["Name"];
+    appSettings.generator.value = _categoryToGenerator(jSettings["Category"]);
+    appSettings.wave.value = _waveToGenerator(jSettings["WaveShape"]);
+    appSettings.sampleRate.value = jSettings["SampleRate"];
+    appSettings.sampleSize.value = jSettings["SampleSize"];
+    appSettings.volume.value = jSettings["SoundVolume"];
 
     // Frequency
     frequencySettings.frequency.value = jSettings["BaseFrequency"];
     frequencySettings.minCutoff.value = jSettings["FrequencyLimit"];
     frequencySettings.slide.value = jSettings["FrequencyRamp"];
     frequencySettings.deltaSlide.value = jSettings["FrequencyDeltaRamp"];
+
+    // Vibrator
+    vibratoSettings.depth.value = jSettings["VibratoStrength"];
+    vibratoSettings.speed.value = jSettings["VibratoSpeed"];
+
+    // Arpeggiation
+    arpeggiationSettings.multiplier.value = jSettings["ArpeggioMod"];
+    arpeggiationSettings.speed.value = jSettings["ArpeggioSpeed"];
+
+    // DutyCycle
+    dutyCycleSettings.dutyCycle.value = jSettings["DutyCycle"];
+    dutyCycleSettings.sweep.value = jSettings["DutyCycleRamp"];
+
+    // Retrigger
+    retriggerSettings.rate.value = jSettings["RepeatSpeed"];
+
+    // Flanger
+    flangerSettings.offset.value = jSettings["FlangerPhaseOffset"];
+    flangerSettings.sweep.value = jSettings["FlangerPhaseRamp"];
+
+    // LowPass
+    lowPassFilterSettings.cutoffFreq.value =
+        jSettings["LowPassFilterFrequency"];
+    lowPassFilterSettings.cutoffSweep.value =
+        jSettings["LowPassFilterFrequencyRamp"];
+    lowPassFilterSettings.resonance.value =
+        jSettings["LowPassFilterFrequencyResonance"];
+
+    // HighPass
+    highPassFilterSettings.cutoffFreq.value =
+        jSettings["HighPassFilterFrequency"];
+    highPassFilterSettings.cutoffSweep.value =
+        jSettings["HighPassFilterFrequencyRamp"];
+
+    // Envelope
+    envelopeSettings.attack.value = jSettings["EnvelopeAttack"];
+    envelopeSettings.sustain.value = jSettings["EnvelopeSustain"];
+    envelopeSettings.punch.value = jSettings["EnvelopePunch"];
+    envelopeSettings.decay.value = jSettings["EnvelopeDecay"];
+  }
+
+  Generator _categoryToGenerator(String cat) {
+    switch (cat) {
+      case "PickUp":
+        return Generator.PickUp;
+      case "Laser":
+        return Generator.Laser;
+      case "Explosion":
+        return Generator.Explosion;
+      case "PowerUp":
+        return Generator.PowerUp;
+      case "Hit":
+        return Generator.Hit;
+      case "Blip":
+        return Generator.Blip;
+      case "Synth":
+        return Generator.Synth;
+      case "Random":
+        return Generator.Random;
+      case "Tone":
+        return Generator.Tone;
+      case "Mutate":
+        return Generator.Mutate;
+      default:
+        return Generator.None;
+    }
+  }
+
+  WaveForm _waveToGenerator(int wave) {
+    switch (wave) {
+      case 0:
+        return WaveForm.Square;
+      case 1:
+        return WaveForm.Triangle;
+      case 2:
+        return WaveForm.Sine;
+      case 3:
+        return WaveForm.SawtoothFalling;
+      case 4:
+        return WaveForm.WhiteNoise;
+      case 5:
+        return WaveForm.PinkNoise;
+      case 6:
+        return WaveForm.RedNoise; // Brownian/red
+      case 7:
+        return WaveForm.SawtoothRising;
+      default:
+        return WaveForm.None;
+    }
   }
 }
